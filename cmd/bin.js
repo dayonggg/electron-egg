b43c81ce11c902075176e98fe732c8b03b9c13c8
dayonggg <83155751@qq.com>
2025-07-31 17:59:04 +0800

    clean

--- cmd/bin.js
+++ cmd/bin.js
@@ -12,14 +12,14 @@ module.exports = {
       directory: './frontend',
       cmd: 'npm',
       args: ['run', 'dev'],
-      port: 8080,
+      port: 8080
     },
     electron: {
       directory: './',
       cmd: 'electron',
       args: ['.', '--env=local'],
       watch: true,
-      delay: 1000,
+      delay: 1000
     }
   },
 
@@ -31,62 +31,36 @@ module.exports = {
     frontend: {
       directory: './frontend',
       cmd: 'npm',
-      args: ['run', 'build'],
+      args: ['run', 'build']
     },
     electron: {
-      type: 'typescript',
+      type: 'typescript'
     },
     win64: {
       cmd: 'electron-builder',
       directory: './',
-      args: ['--config=./cmd/builder.json', '-w=nsis', '--x64'],
+      args: ['--config=./cmd/builder.json', '-w=nsis', '--x64']
     },
     win32: {
-      args: ['--config=./cmd/builder.json', '-w=nsis', '--ia32'],
+      args: ['--config=./cmd/builder.json', '-w=nsis', '--ia32']
     },
     win_e: {
-      args: ['--config=./cmd/builder.json', '-w=portable', '--x64'],
+      args: ['--config=./cmd/builder.json', '-w=portable', '--x64']
     },
     win_7z: {
-      args: ['--config=./cmd/builder.json', '-w=7z', '--x64'],
+      args: ['--config=./cmd/builder.json', '-w=7z', '--x64']
     },
     mac: {
-      args: ['--config=./cmd/builder-mac.json', '-m'],
+      args: ['--config=./cmd/builder-mac.json', '-m']
     },
     mac_arm64: {
-      args: ['--config=./cmd/builder-mac-arm64.json', '-m', '--arm64'],
-    },
-    linux: {
-      args: ['--config=./cmd/builder-linux.json', '-l=deb', '--x64'],
-    },
-    linux_arm64: {
-      args: ['--config=./cmd/builder-linux.json', '-l=deb', '--arm64'],
-    },
-    go_w: {
-      directory: './go',
-      cmd: 'go',
-      args: ['build', '-o=../build/extraResources/goapp.exe'],
-    },
-    go_m: {
-      directory: './go',
-      cmd: 'go',
-      args: ['build', '-o=../build/extraResources/goapp'],
-    },
-    go_l: {
-      directory: './go',
-      cmd: 'go',
-      args: ['build', '-o=../build/extraResources/goapp'],
-    },
-    python: {
-      directory: './python',
-      cmd: 'python',
-      args: ['./setup.py', 'build'],
-    },
+      args: ['--config=./cmd/builder-mac-arm64.json', '-m', '--arm64']
+    }
   },
 
   /**
    * 移动资源
-   * ee-bin move 
+   * ee-bin move
    */
   move: {
     frontend_dist: {
@@ -112,8 +86,8 @@ module.exports = {
     python_dist: {
       src: './python/dist',
       dest: './build/extraResources/py'
-    },
-  },  
+    }
+  },
 
   /**
    * 预发布模式（prod）
@@ -127,41 +101,37 @@ module.exports = {
 
   /**
    * 加密
-   */  
+   */
   encrypt: {
     frontend: {
       type: 'none',
-      files: [
-        './public/dist/**/*.(js|json)',
-      ],
+      files: ['./public/dist/**/*.(js|json)'],
       cleanFiles: ['./public/dist'],
       confusionOptions: {
-        compact: true,      
+        compact: true,
         stringArray: true,
         stringArrayEncoding: ['none'],
         stringArrayCallsTransform: true,
         numbersToExpressions: true,
-        target: 'browser',
+        target: 'browser'
       }
     },
     electron: {
       type: 'confusion',
-      files: [
-        './public/electron/**/*.(js|json)',
-      ],
+      files: ['./public/electron/**/*.(js|json)'],
       cleanFiles: ['./public/electron'],
       specificFiles: [
         './public/electron/main.js',
-        './public/electron/preload/bridge.js',
+        './public/electron/preload/bridge.js'
       ],
       confusionOptions: {
-        compact: true,      
+        compact: true,
         stringArray: true,
         stringArrayEncoding: ['none'],
         deadCodeInjection: false,
         stringArrayCallsTransform: true,
         numbersToExpressions: true,
-        target: 'node',
+        target: 'node'
       }
     }
   },
@@ -170,30 +140,5 @@ module.exports = {
    * 执行自定义命令
    * ee-bin exec
    */
-  exec: {
-    // 单独调试，air 实现 go 热重载
-    go: {
-      directory: './go',
-      cmd: 'air',
-      args: ['-c=config/.air.toml' ],
-    },
-    // windows 单独调试，air 实现 go 热重载 
-    go_w: {
-      directory: './go',
-      cmd: 'air',
-      args: ['-c=config/.air.windows.toml' ],
-    },    
-    // 单独调试，以基础方式启动 go
-    go2: {
-      directory: './go',
-      cmd: 'go',
-      args: ['run', './main.go', '--env=dev','--basedir=../', '--port=7073'],
-    },     
-    python: {
-      directory: './python',
-      cmd: 'python',
-      args: ['./main.py', '--port=7074'],
-      stdio: "inherit", // ignore
-    },
-  },  
-};
+  exec: {}
+}
